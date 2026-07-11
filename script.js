ce35b5703b6cdc293d54b425c6eee5d243c0321f
nmd216129-coder <nmd216129@gmail.com>
2026-07-11 19:52:49 +0600

    script.js

--- script.js
+++ script.js
@@ -13,19 +13,6 @@ if (withdrawBtn) {
     });
 }
 
-const actions = document.querySelectorAll(".action");
-
-actions.forEach(action => {
-    action.addEventListener("click", () => {
-        // a ট্যাগ হওয়ায় href নিজে থেকেই কাজ করবে
-    });
-});
-
-const balance = document.getElementById("balance");
-
-if (balance) {
-    balance.innerText = "৳50.00";
-}
 function openFacebook() {
     window.open("https://www.facebook.com/share/1ERvH8URum/", "_blank");
 }
