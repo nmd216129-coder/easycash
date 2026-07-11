ea3dfd095444324ee69fe8e239e43af482da715f
nmd216129-coder <nmd216129@gmail.com>
2026-07-11 19:38:38 +0600

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA1ZhfhVDUSuMJBso1HOSdrgyVecneEW7Y",
@@ -15,3 +21,36 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 window.db = db;
+
+const tg = window.Telegram?.WebApp;
+
+if (tg) {
+  tg.ready();
+
+  const user = tg.initDataUnsafe?.user;
+
+  if (user) {
+    createUser(user);
+  }
+}
+
+async function createUser(user) {
+
+  const userRef = doc(db, "users", String(user.id));
+
+  const snap = await getDoc(userRef);
+
+  if (!snap.exists()) {
+
+    await setDoc(userRef, {
+      id: user.id,
+      name: user.first_name,
+      username: user.username || "",
+      balance: 50,
+      referral: 0,
+      joined: new Date().toISOString()
+    });
+
+  }
+
+}
