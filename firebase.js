057828d733617ea407da16d73af2ef559485191b
nmd216129-coder <nmd216129@gmail.com>
2026-07-16 20:44:53 +0600

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,55 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc,
-  updateDoc,
-  increment
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA1ZhfhVDUSuMJBso1HOSdrgyVecneEW7Y",
-  authDomain: "easycash-f6a8e.firebaseapp.com",
-  projectId: "easycash-f6a8e",
-  storageBucket: "easycash-f6a8e.firebasestorage.app",
-  messagingSenderId: "699730178527",
-  appId: "1:699730178527:web:62eb4605ddd1acacde146a",
-  measurementId: "G-8BKYR0GY09"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-window.db = db;
-
-const tg = window.Telegram?.WebApp;
-
-if (tg) {
-  tg.ready();
-
-  const user = tg.initDataUnsafe?.user;
-
-  if (user) {
-    createUser(user);
-  }
-}
-
-
 async function createUser(user) {
 
   const userRef = doc(db, "users", String(user.id));
-
   const snap = await getDoc(userRef);
 
   if (!snap.exists()) {
 
     const refCode = "EC" + user.id;
 
+    // Referral parameter
+    let referredBy = tg.initDataUnsafe?.start_param || "";
+
     console.log("Telegram Data:", tg.initDataUnsafe);
-console.log("Start Param:", tg.initDataUnsafe?.start_param);
+    console.log("Start Param:", referredBy);
 
     await setDoc(userRef, {
       id: user.id,
@@ -63,17 +25,15 @@ console.log("Start Param:", tg.initDataUnsafe?.start_param);
     });
 
     if (referredBy) {
-
       const refUserRef = doc(db, "users", referredBy.replace("EC", ""));
 
-      await updateDoc(refUserRef, {
-        balance: increment(30),
-        referral: increment(1)
-      });
-
+      const refSnap = await getDoc(refUserRef);
+      if (refSnap.exists()) {
+        await updateDoc(refUserRef, {
+          balance: increment(30),
+          referral: increment(1)
+        });
+      }
     }
-
   }
-
 }
-export { db };
