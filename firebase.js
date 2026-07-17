16a6a94b0e8bad012c1a54ff8f7f65be0ab24144
nmd216129-coder <nmd216129@gmail.com>
2026-07-17 18:14:37 +0600

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,39 +1,39 @@
-async function createUser(user) {
 
-  const userRef = doc(db, "users", String(user.id));
-  const snap = await getDoc(userRef);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-  if (!snap.exists()) {
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  increment
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-    const refCode = "EC" + user.id;
+const firebaseConfig = {
+  apiKey: "AIzaSyA1ZhfhVDUSuMJBso1HOSdrgyVecneEW7Y",
+  authDomain: "easycash-f6a8e.firebaseapp.com",
+  projectId: "easycash-f6a8e",
+  storageBucket: "easycash-f6a8e.firebasestorage.app",
+  messagingSenderId: "699730178527",
+  appId: "1:699730178527:web:62eb4605ddd1acacde146a",
+  measurementId: "G-8BKYR0GY09"
+};
 
-    // Referral parameter
-    let referredBy = tg.initDataUnsafe?.start_param || "";
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-    console.log("Telegram Data:", tg.initDataUnsafe);
-    console.log("Start Param:", referredBy);
+export { db };
 
-    await setDoc(userRef, {
-      id: user.id,
-      name: user.first_name,
-      username: user.username || "",
-      balance: referredBy ? 20 : 50,
-      referral: 0,
-      refCode: refCode,
-      referredBy: referredBy,
-      joined: new Date().toISOString()
-    });
+const tg = window.Telegram?.WebApp;
 
-    if (referredBy) {
-      const refUserRef = doc(db, "users", referredBy.replace("EC", ""));
+if (tg) {
+  tg.ready();
+  tg.expand();
 
-      const refSnap = await getDoc(refUserRef);
-      if (refSnap.exists()) {
-        await updateDoc(refUserRef, {
-          balance: increment(30),
-          referral: increment(1)
-        });
-      }
-    }
+  const user = tg.initDataUnsafe?.user;
+
+  if (user) {
+    createUser(user);
   }
 }
