9f92b5c238d267b889000f0e14fd174836f01cea
nmd216129-coder <nmd216129@gmail.com>
2026-07-17 18:40:52 +0600

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
+import { db } from "./firebase.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 const tg = window.Telegram?.WebApp;
 
 if (tg) {
-    tg.ready();
-    tg.expand();
+  tg.ready();
+  tg.expand();
 }
+
+async function loadBalance() {
+
+  const user = tg?.initDataUnsafe?.user;
+
+  if (!user) return;
+
+  const userRef = doc(db, "users", String(user.id));
+  const snap = await getDoc(userRef);
+
+  if (snap.exists()) {
+
+    const data = snap.data();
+
+    const balance = document.getElementById("balance");
+
+    if (balance) {
+      balance.innerText = "৳" + data.balance;
+    }
+
+  }
+
+}
+
+loadBalance();
 
 const withdrawBtn = document.querySelector(".balance-card button");
 
 if (withdrawBtn) {
-    withdrawBtn.addEventListener("click", () => {
-        window.location.href = "withdraw.html";
-    });
+  withdrawBtn.addEventListener("click", () => {
+    window.location.href = "withdraw.html";
+  });
 }
 
-function openFacebook() {
-    window.open("https://www.facebook.com/share/1ERvH8URum/", "_blank");
-}
+window.openFacebook = function () {
+  window.open("https://www.facebook.com/share/1ERvH8URum/", "_blank");
+};
 
-function openYouTube() {
-    window.open("https://www.youtube.com/@EasyCash-y2c", "_blank");
-}
+window.openYouTube = function () {
+  window.open("https://www.youtube.com/@EasyCash-y2c", "_blank");
+};
